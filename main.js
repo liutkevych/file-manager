fc88d4883c589410ccceb35bd1946fd622ab1e09
liutkevych <oleksandr.liutkevych@seniordevonly.com>
2018-01-09 20:22:24 +0200

    Add rename and delete methods

--- main.js
+++ main.js
@@ -1,50 +1,56 @@
-(function () {
-  function ready () {
 // The store for created elements
-    var resultElements = {
-      items: [{}]
-    };
+var resultElements = {
+  items: [{}]
+};
 
-    var filesTempate = document.getElementById('files-template').innerHTML;
-    var compiledFilesTemplate = Handlebars.compile(filesTempate);
+var filesTempate = document.getElementById('files-template').innerHTML;
+var compiledFilesTemplate = Handlebars.compile(filesTempate);
+
+function updatedHtml () {
+  return document.getElementById('created-items').innerHTML = compiledFilesTemplate(resultElements);
+}
 
 // Constructor for brand new items
-    var Item = function (name, isFolder) {
-      this.name = name,
-      this.isFolder = isFolder,
-      this.date = new Date(),
-      remameItem = function () {
-        console.log(`Use method remameItem`);
-      },
-      deleteItem = function () {
-        console.log(`Use method deleteItem`);
-      }
-    }
+function Item (name, isFolder) {
+  this.name = name,
+  this.isFolder = isFolder,
+  this.date = new Date().toLocaleString()
+};
+
+function renameItem (index) {
+  newName = prompt('Enter new name');
+  resultElements.items[index].name = newName;
+  updatedHtml();
+};
+
+function deleteItem (index) {
+  resultElements.items.splice(index, 1);
+  updatedHtml();
+};
+
+function createFolder () {
+  var name = prompt('Enter folder name');
+  var item = new Item(name, true);
+
+  resultElements.items.push(item);
+  updatedHtml();
+  console.log(resultElements.items);
+};
+
+function createFile () {
+  var name = prompt('Enter file name');
+  var item = new Item(name, false);
+
+  resultElements.items.push(item);
+  updatedHtml();
+};
+
+function applyFilters() {
+  var pickedDate = document.getElementById('date').value;
+  console.log(pickedDate);
+};
 
 // Add ivents listners
-    document.getElementById('newFile').addEventListener('click', createFolder);
-    document.getElementById('newFolder').addEventListener('click', createFolder);
-    document.getElementById('apply').addEventListener('click', applyFilters)
-
-
-    function createFolder () {
-      var name = prompt('Enter file name');
-      var item = new Item(name, true);
-
-      resultElements.items.push(item);
-      document.getElementById('created-items').innerHTML = compiledFilesTemplate(resultElements);
-      console.log(resultElements.items);
-      console.log(this);
-    };
-
-    function applyFilters() {
-      var pickedDate = document.getElementById('date').value;
-      console.log(pickedDate);
-    };
-
-    function rename () {
-      console.log(this);
-    };
-  };
-  document.addEventListener("DOMContentLoaded", ready);
-}());
+document.getElementById('newFile').addEventListener('click', createFile);
+document.getElementById('newFolder').addEventListener('click', createFolder);
+document.getElementById('apply').addEventListener('click', applyFilters)
