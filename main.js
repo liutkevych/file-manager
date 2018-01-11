f1ccae2e1cd231d0686f1f338c5c68b652320fb2
liutkevych <oleksandr.liutkevych@seniordevonly.com>
2018-01-11 17:59:52 +0200

    Add filtration by date and by type, add some comments

--- main.js
+++ main.js
@@ -6,9 +6,10 @@ var resultElements = {
 var filesTempate = document.getElementById('files-template').innerHTML;
 var compiledFilesTemplate = Handlebars.compile(filesTempate);
 
+
 function updatedHtml () {
   return document.getElementById('created-items').innerHTML = compiledFilesTemplate(resultElements);
-}
+};
 
 // Constructor for brand new items
 function Item (name, isFolder) {
@@ -17,6 +18,7 @@ function Item (name, isFolder) {
   this.date = new Date().toLocaleString()
 };
 
+// CRUD functions
 function renameItem (index) {
   newName = prompt('Enter new name');
   resultElements.items[index].name = newName;
@@ -34,7 +36,6 @@ function createFolder () {
 
   resultElements.items.push(item);
   updatedHtml();
-  console.log(resultElements.items);
 };
 
 function createFile () {
@@ -45,9 +46,54 @@ function createFile () {
   updatedHtml();
 };
 
+// Filtering elements
 function applyFilters() {
   var pickedDate = document.getElementById('date').value;
-  console.log(pickedDate);
+  var baseDate = new Date(pickedDate).toLocaleString();
+  var filteredElements = {
+    items: [{}]
+  };
+
+  function filteredHtml () {
+    return document.getElementById('created-items').innerHTML = compiledFilesTemplate(filteredElements);
+  };
+
+  // Function for filtering by the date of cteating
+  function dateFilter () {
+    filteredElements.items = resultElements.items.filter(function(item){
+      return item.date > baseDate;
+    });
+    filteredHtml();
+  };
+
+  // Function for filtering if element is folder or not
+  function typeFilter () {
+    filteredElements.items = resultElements.items.filter(function(item){
+      return item.isFolder === true;
+    });
+    filteredHtml();
+  };
+
+  function deepFilter() {
+    filteredElements.items = resultElements.items.filter(function(item){
+      return (item.isFolder === true && item.date > baseDate);
+    });
+    filteredHtml();
+  }
+
+  if (!document.getElementById('showFolders').checked && !document.getElementById('date').value) {
+    alert('Please use some filters');
+    updatedHtml();
+  } else if (!document.getElementById('showFolders').checked) {
+    console.log('use dateFilter()');
+    dateFilter();
+  } else if (!document.getElementById('date').value) {
+    console.log('use typeFilter()');
+    typeFilter();
+  } else {
+    console.log('use both filter');
+    deepFilter();
+  }
 };
 
 // Add ivents listners
